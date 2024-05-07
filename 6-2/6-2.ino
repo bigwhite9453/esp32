063c0e68072f2ba6c4c6304ec6201c63187b6fc3
bigwhite9453 <152352566+bigwhite9453@users.noreply.github.com>
2024-05-07 16:00:47 +0800

    Add files via upload

---
+++ 6-2/6-2.ino
@@ -0,0 +1,32 @@
+#include <Keypad.h>
+
+const byte _7SegPin = [12,14,27,26,25,33,32];
+const byte ROWS = 3;
+const byte COLS = 3;
+
+char keys [ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  
+};
+byte rowPins[ROWS] = {32,33,25};
+byte colPins[COLS] = {27,14,12};
+Keypad keypad = Keypad(makeKeymap(keys),rowPins, colPins, ROWS, COLS);
+
+void setup() {
+Serial.begin(9600);
+for (int i=0;i<8;i++){
+  pinMode (_7SegPin[i], OUTPUT);
+}
+}
+
+void loop() {
+  char key = keypad.getKey();
+  if (key) {
+    Serial.println(key);
+  }
+  if (key == '1') {
+    
+  }
+}
