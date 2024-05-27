5598525d4c3cbe289bb80f411c066bc5a79fd5fa
bigwhite9453 <152352566+bigwhite9453@users.noreply.github.com>
2024-05-28 03:59:38 +0800

    Update 6-2.ino

--- 6-2/6-2.ino
+++ 6-2/6-2.ino
@@ -29,4 +29,31 @@ void loop() {
   if (key == '1') {
     
   }
+  else if (key == '2'){
+
+  }
+  else if (key == '3'){
+    
+  }
+  else if (key == '4'){
+    
+  }
+  else if (key == '5'){
+    
+  }
+  else if (key == '6'){
+    
+  }
+  else if (key == '7'){
+    
+  }
+  else if (key == '8'){
+    
+  }
+  else if (key == '9'){
+    
+  }
 }
+
+  
+
